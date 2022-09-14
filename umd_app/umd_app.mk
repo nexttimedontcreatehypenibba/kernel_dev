d6e42940d43a113070b29bf84e3b86b604854af6
Five Years and HYDERABAD Qualcomm LOL <113478093+nexttimedontcreatehypenibba@users.noreply.github.com>
2022-09-14 14:38:32 +0530

    Update umd_app.mk

--- umd_app/umd_app.mk
+++ umd_app/umd_app.mk
@@ -1,13 +1,1 @@
-UMD_APP_VERSION = 1.0
-UMD_APP_SITE = ./package/umd_app/src
-UMD_APP_SITE_METHOD = local
-
-define UMD_APP_BUILD_CMDS
-    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D)
-endef
-
-define UMD_APP_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/umd_app $(TARGET_DIR)/usr/bin
-endef
-
-$(eval $(generic-package))
+Don't create so much hype nibba. It makes you look laughable.
